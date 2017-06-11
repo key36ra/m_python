d2b50378ea190a5139a665be02954b331970b562
key36ra <key_36ra@outlook.jp>
2017-06-11 09:06:09 +0900

    6/11 first commit

--- seireki-gengou.py
+++ seireki-gengou.py
@@ -12,7 +12,10 @@ now = datetime.now()
 
 print("あなたの生まれ年(西暦)を元号に変換します")
 
-year = input("生まれ年:")
+# python3では、input()はstr型になっている(python2はint型)
+year = int(input("生まれ年:"))
+
+print()
 
 if year in range(1868, now.year):
     print("あなたの生まれ年は、元号で表すと、")
@@ -20,9 +23,10 @@ if year in range(1868, now.year):
         print("明治{}年".format(year-1867))
     elif year in range(1913, 1926):
         print("大正{}年".format(year-1911))
-    elif year in range(1226, 1989):
+    elif year in range(1926, 1989):
         print("昭和{}年".format(year-1925))
-    elif year in range(1989, 2017):
+    elif year in range(1989, now.year+1):
         print("平成{}年".format(year-1988))
+    print("です。")
 else:
-    print("ごめんなさい。明治以降しかわかりません!(_ _)!")
+    print("すんまへんな。明治から平成しかわかりまへんで!(_ _)!")
